e8c52c34c9700724fd2f16c24aa4e773e045753e
rushitgit <f20210010@dubai.bits-pilani.ac.in>
2023-12-19 12:05:26 +0400

    added nlp

---
+++ NLP/nlp2.py
@@ -0,0 +1,81 @@
+import csv
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from textblob import TextBlob
+from nltk.sentiment import SentimentIntensityAnalyzer
+import numpy as np
+top_players_data = pd.read_csv(r'datasets\TopPlayers.csv')
+
+
+articles_data = pd.read_csv(r'datasets\articles_set.csv', encoding='latin1')
+
+# Load the articles dataset from the CSV file
+
+# ... (rest of the code)
+
+# Extract unique player names from the top players dataset
+unique_players = top_players_data['PLAYER'].unique()
+
+# Initialize sentiment scores
+sentiment_scores = []
+
+# Perform sentiment analysis for each player
+for player_name in unique_players:
+    # Filter articles data for the specific player
+    player_articles = articles_data[articles_data['text'].str.contains(player_name, case=False, na=False)]
+
+    # Calculate sentiment score
+    if not player_articles.empty:
+        # Combine all articles for the player
+        combined_text = ' '.join(player_articles['text'])
+
+        # Use TextBlob for sentiment analysis
+        blob = TextBlob(combined_text)
+        sentiment_score = blob.sentiment.polarity
+
+        # Or use NLTK's SentimentIntensityAnalyzer
+        # sia = SentimentIntensityAnalyzer()
+        # sentiment_score = sia.polarity_scores(combined_text)['compound']
+
+        sentiment_scores.append({'PLAYER': player_name, 'Sentiment_Score': sentiment_score})
+    else:
+        sentiment_scores.append({'PLAYER': player_name, 'Sentiment_Score': None})
+
+# Convert sentiment scores to a DataFrame
+sentiment_df = pd.DataFrame(sentiment_scores)
+
+# Merge sentiment scores with top players data
+top_players_with_sentiment = pd.merge(top_players_data, sentiment_df, on='PLAYER', how='left')
+
+# Print the result
+print(top_players_with_sentiment)
+
+
+
+
+
+
+
+# Select relevant columns
+top_players_sentiments_df = top_players_with_sentiment[['PLAYER', 'TEAM', 'POSITION','COST','OWNERSHIP %', 'PREDICTED_POINTS','Sentiment_Score']]
+
+conditions = [
+    (top_players_sentiments_df['Sentiment_Score'] > 0),
+    (top_players_sentiments_df['Sentiment_Score'] == 0) | (top_players_sentiments_df['Sentiment_Score'].isna()),
+    (top_players_sentiments_df['Sentiment_Score'] < 0)
+]
+
+choices = ['positive', 'neutral', 'negative']
+
+top_players_sentiments_df['POSITIVITY'] = np.select(conditions, choices, default='neutral')
+
+
+print(top_players_sentiments_df)
+
+top_players_sentiments_df.to_csv('datasets\TopPlayersWithSentiments.csv')
+
+
+
