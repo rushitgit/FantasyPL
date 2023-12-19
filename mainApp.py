bd137a503c983477ab21057e3d22a25723d01453
rushitgit <f20210010@dubai.bits-pilani.ac.in>
2023-12-19 14:28:17 +0400

    updated streamlit

--- mainApp.py
+++ mainApp.py
@@ -29,14 +29,28 @@ with col1:
 with col2:
     st.markdown("<h1 style='text-align: center; color: black;'></h1>", unsafe_allow_html=True)
     st.markdown("<h1 style='text-align: center; color: black;'>Fantasy Premier League</h1>", unsafe_allow_html=True)
+    st.markdown("<h1 style='text-align: center; color: black;'></h1>", unsafe_allow_html=True)
+    # st.markdown("<div style='text-align: center; color: black; border: 2px solid rgba(0, 0, 0, 0.3); border-radius: 10px; padding: 10px; margin: 10px;'>"
+    #             "<h2 style= 'color: black;'>Average: 40</h2>"
+    #             "<h2 style= 'color: black;'>Score: 60</h2>"
+    #             "</div>", unsafe_allow_html=True)
+    st.markdown("<h2 style='text-align: center; color: black;'>Average:40</h2>", unsafe_allow_html=True)
+    st.markdown("<h2 style='text-align: center; color: black;'>Score:60</h2>", unsafe_allow_html=True)
+    st.markdown("<h1 style='text-align: center; color: black;'></h1>", unsafe_allow_html=True)
 
 #st.title("Fantasy Premier League")
 
 
 col3,col4,col5,col6 = st.columns((4,6,30,6))
 with col4:
-    st.image('images/custom.png', width= 500)
+    st.image('images/final_team.jpg', width= 500)
     #st.image('teamnew.png')
+    st.markdown("<h2 style='text-align: left; color: black;'>Substitutes:</h2>", unsafe_allow_html=True) 
+    st.markdown("<h5 style='text-align: left; color: black;'>Neto(4.6m)</h5>", unsafe_allow_html=True) 
+    st.markdown("<h5 style='text-align: left; color: black;'>Mainoo(4.4m)</h5>", unsafe_allow_html=True) 
+    st.markdown("<h5 style='text-align: left; color: black;'>Gusto(4.1m)</h5>", unsafe_allow_html=True) 
+    st.markdown("<h5 style='text-align: left; color: black;'>Kelleher(4.0m)</h5>", unsafe_allow_html=True) 
+
 
 with col6:
    st.markdown("<h1 style='text-align: center; color: white;'>News</h2>", unsafe_allow_html=True) 
@@ -50,5 +64,9 @@ st.image('images/debruyne.png')
 st.image('images/miley.png')
 
 
-video_path = "/Users/yohan/AI 3/FantasyPL/images/My Movie.mp4"
+video_path = "images\My Movie.mp4"
 st.video(video_path)
+
+
+st.markdown("[Redirect to Chatbot](http://localhost:8502)")
+
