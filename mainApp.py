dc2ae5432898c256b8ab28bcad1c22a7a955d3e2
Yo-han-K <ygk2801@gmail.com>
2023-12-19 11:49:18 +0400

    files upload

--- mainApp.py
+++ mainApp.py
@@ -33,13 +33,22 @@ with col2:
 #st.title("Fantasy Premier League")
 
 
-col3,col4 = st.columns((1,5))
-
+col3,col4,col5,col6 = st.columns((4,6,30,6))
 with col4:
     st.image('images/custom.png', width= 500)
     #st.image('teamnew.png')
 
+with col6:
+   st.markdown("<h1 style='text-align: center; color: white;'>News</h2>", unsafe_allow_html=True) 
+   st.image('images/fplnews.png', width= 225)
+   st.image('images/rightnews.png', width= 225)
+   st.image('images/leftnews1.png', width= 225) 
+
+st.image('images/pep.png')
+st.image('images/palmer.png')
+st.image('images/debruyne.png')
+st.image('images/miley.png')
 
 
-
-st.write ("If I need Cameron Archer to come on, then that's what he's there for. Yeah, right. Oscar, your team's up next. Now, of course, you told us you'd already sold Haaland. So, who have you brought in to replace him? Drumroll. Yeah, it was Solanke. Solanke’s come in for a hit. Yeah, I think I've already had Watkins. I think he's just the obvious pick with those fixtures. He's second for expected goal-in-a-bombing over the last four game weeks. He's taken 14 shots in the box, which is topping the league over that run.")
+video_path = "/Users/yohan/AI 3/FantasyPL/images/My Movie.mp4"
+st.video(video_path)
