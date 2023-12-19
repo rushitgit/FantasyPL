84c3a40dd5e91d36a4d23744573fefd7c36d31d0
Yo-han-K <ygk2801@gmail.com>
2023-12-19 08:46:33 +0400

    testing

--- mainApp.py
+++ mainApp.py
@@ -38,3 +38,8 @@ col3,col4 = st.columns((1,5))
 with col4:
     st.image('images/custom.png', width= 500)
     #st.image('teamnew.png')
+
+
+
+
+st.write ("If I need Cameron Archer to come on, then that's what he's there for. Yeah, right. Oscar, your team's up next. Now, of course, you told us you'd already sold Haaland. So, who have you brought in to replace him? Drumroll. Yeah, it was Solanke. Solanke’s come in for a hit. Yeah, I think I've already had Watkins. I think he's just the obvious pick with those fixtures. He's second for expected goal-in-a-bombing over the last four game weeks. He's taken 14 shots in the box, which is topping the league over that run.")
